7567456d2c5424f77deaf4c4d128fed13ccdc7a2
HongLong40 <37321971+HongLong40@users.noreply.github.com>
2021-03-20 09:50:52 +0100

    dotfiles update 2021-03-20 09:50:52+01:00

--- .config/polybar/check_pacman_updates.sh
+++ .config/polybar/check_pacman_updates.sh
@@ -20,8 +20,9 @@ echo $(date "+%Y-%m-%d %T") Starting $0 with PID = $$ > $LOG
 echo $(date "+%Y-%m-%d %T") Polling Interval = ${pbc[poll_interval]} >> $LOG
 
 typeset -i last_update_count=0
-typeset -i curr_update_count
+typeset -i curr_update_count=0
 typeset -i counter=1
+typeset -i first_run=1
 
 # Wait for 15s, as polybars need to start up
 while [[ (! -f /tmp/polybar_started) && $counter -le 15 ]]; do
@@ -34,10 +35,11 @@ while true; do
     curr_update_count=$(checkupdates 2> /dev/null | wc -l | tee /tmp/pacman_updates.count)
     #curr_update_count=$(cat /tmp/pacman_updates.count)
     echo $(date "+%Y-%m-%d %T") Current Update Count: ..$curr_update_count.. >> $LOG
-    if [[ $last_update_count != $curr_update_count ]]; then
+    if [[ ($last_update_count -ne $curr_update_count) || $first_run -eq 1 ]]; then
         last_update_count=$curr_update_count
         polybar-msg hook ${pbc[hook_name]} ${pbc[hook_id]} || true
         dunstify -t 5000 "$curr_update_count New Pacman Updates Available"
+        first_run=0
     fi
     sleep ${pbc[poll_interval]}
 done
